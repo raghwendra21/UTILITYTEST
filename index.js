d95e4a41faab635e6f3997df114bd4dd8c7cdace
raghwendra <raghwendrap@siliconasiaworks.com>
2022-04-11 18:52:04 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,6 @@
+var add = require('./services/testFile').add
+var add1 = require('./services/testFile').add1
+var add2 = require('./services/testFile').add2
+
+
+module.exports = { add, add1, add2 }
---
+++ services/testFile.js
@@ -0,0 +1,13 @@
+const add = (a, b) => {
+    return a + b
+}
+
+const add1 = (a, b) => {
+    return a + b
+}
+
+const add2 = (a, b) => {
+    return a + b
+}
+
+module.exports = { add, add1, add2 }
